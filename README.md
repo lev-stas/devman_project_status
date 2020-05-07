c4a35b6a326b79a246d212fc9db0208e7b0352f5
Stanislav Levchenko <staslevchenko@protonmail.com>
2020-05-07 21:33:35 +0300

    The second commit, after the first review - the names of variables in project_status.py were srecified; - environment variables were specified; - connection attempt were delayed after ReadTimeout exception; - global variables were removed from function; - README file was edited.

--- README.md
+++ README.md
@@ -2,10 +2,35 @@
 This project creates telegram bot which notifies when your project is checked by DevMav expert.
 
 #### Dependences
-You need Python3 interpreter to use this script. All needed dependences are included in requirements.txt file. Use ```pip install -r requirements.txt``` to install them (use ```pip3``` instead ```pip```  if you have both python2 and python3 versions).
+You need Python3 interpreter to use this script. All needed dependences are included in requirements.txt file. Use 
+```pip install -r requirements.txt```
+ to install them (use `pip3` instead `pip`  if you have both python2 and python3 versions).
 
 #### Usage
+To run this script, type in console, beeing in the same directory, where project_status.py located:
+```python project_status.py &```
+Use `python3` instead `python` if you have both python2 and python3 versions.
 When you run project_status.py, it will work until your server power off or until you`ll kill a process. It will send you messages in telegram every time, your project status changes.
+
+This script allows to use telegram bot even in case telegram is blocked in your country. To make it work you need socks5 server. If telegram is not blocked in your country, you can remove 
+``` telegram_proxy = telegram.utils.request.Request(proxy_url=f'socks5h://{proxy_login}:{proxy_password}@{proxy_url}') ```
+and replace
+``` bot = telegram.Bot(token=telegram_token) ```
+isntead of 
+```bot = telegram.Bot(token=telegram_token, request = telegram_proxy)```
+
+
+#### Environment variables
+This script needs some environment variables for correct work. Create file .env in the same directory, where project_status.py located with such content:
+``` 
+DEVMAN_TOKEN = <your personal token> on [Devman](https://dvmn.org/api/docs/)
+TELEGRAM_BOT_TOKEN = <your telegram bot token>
+SOCKS5_LOGIN = <login of your socks5 servers> (if exists)
+SOCKS5_PASSWORD = <password of your socks5 server> (if exists)
+SOCKS5_SERVER_URL = <your socks5 server url:port> (if exists)
+CHAT_ID = <yout telegram chat_id> 
+```
+
 
 #### Purpose of project
 This script was performed as a part of API web-services cource by [Devman](https://dvmn.org/modules)
--- project_status.py
+++ project_status.py
@@ -2,33 +2,39 @@ import requests
 import telegram
 from dotenv import load_dotenv
 import os
+import time
 
 DEVMAN_API_URL = 'https://dvmn.org/api/long_polling/'
 
-def check_project_status():
+def check_project_status(url):
     params = {}
+    request_attempt = 0
     while True:
         try:
-            response = requests.get(DEVMAN_API_URL, headers=devman_api_headers, params = params)
+            response = requests.get(url, headers=devman_api_headers, params = params)
             response.raise_for_status()
             devman_answer = response.json()
             if devman_answer ['status'] == 'timeout':
                 params['timestamp'] = devman_answer['timestamp_to_request']
             else:
                 params['timestamp'] = devman_answer['last_attempt_timestamp']
-                attempt_info = devman_answer['new_attempts'][0]
-                negative_status = attempt_info['is_negative']
-                lesson_title = attempt_info['lesson_title']
-                if negative_status:
+                info = devman_answer['new_attempts'][0]
+                status = info['is_negative']
+                lesson_title = info['lesson_title']
+                if status:
                     text = f'Преподаватель проверил проект "{lesson_title}". Есть замечания.'
                 else:
                     text = f'Преподаватель проверил проект "{lesson_title}". Проект принят.'
-                bot.send_message(chat_id = chat_id, text = text)
+                bot.send_message(chat_id = telegram_chat_id, text = text)
+            request_attempt = 0
 
         except requests.exceptions.ReadTimeout:
-            bot.send_message(chat_id = chat_id, text = 'Сервер devman не отвечает более 90 секунд')
+            request_attempt += 1
+            if request_attempt > 3:
+                time.sleep(300)
             continue
         except requests.exceptions.ConnectionError:
+            time.sleep(60)
             continue
 
 if __name__ == '__main__':
@@ -38,14 +44,14 @@ if __name__ == '__main__':
     proxy_password = os.getenv('SOCKS5_PASSWORD')
     proxy_url = os.getenv('SOCKS5_SERVER_URL')
     telegram_token = os.getenv('TELEGRAM_BOT_TOKEN')
-    chat_id = os.getenv('CHAT_ID')
+    telegram_chat_id = os.getenv('CHAT_ID')
 
-    devman_api_headers = {'Authorization' : devman_token}
+    devman_api_headers = {'Authorization' : f'Token {devman_token}'}
     telegram_proxy = telegram.utils.request.Request(proxy_url=f'socks5h://{proxy_login}:{proxy_password}@{proxy_url}')
     bot = telegram.Bot(token=telegram_token, request = telegram_proxy)
 
-    check_project_status()
-
+    check_project_status(DEVMAN_API_URL)
+ 
 
 
 
